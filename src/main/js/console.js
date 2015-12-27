23b7cc9045214a703b68e0486c04dcef4fd16b20
Didier Plaindoux <d.plaindoux@free.fr>
2015-12-27 23:47:12 +0100

    Last version with embedded native libraries

---
+++ src/main/js/console.js
@@ -0,0 +1,29 @@
+/*
+ * Thicket
+ * https://github.com/d-plaindoux/thicket
+ *
+ * Copyright (c) 2015 Didier Plaindoux
+ * Licensed under the LGPL2 license.
+ */
+
+module.exports = function(runtime) {
+    
+    'use strict';
+
+    var $i = runtime.instruction;
+        
+    // ------------------------------
+    // Console
+    // ------------------------------
+    
+    runtime.native("console.log", 2, function(env) {
+        var v1 = runtime.constant(env.pop()),
+            self = env.pop();
+        
+        console.log(v1);
+        
+        return [[ $i.RESULT, self ]];
+    });
+
+};
+ 
